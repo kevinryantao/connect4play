e97d4081a19ce88204859f21d31d374ef094bc8c
Kevin Tao <ktao@groupon.com>
2013-04-20 14:16:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Connect4play::Application.config.secret_token = '0fa6908ba2f0bb6ccfe5f36a23a604bddcbbf052aa0f77a8f1422fa49831f8c807b79e03c0e4aa4523d497d6c38524159098ebcede60fb62b2e6a81589f6261d'
